37732d09749d77385d3cc79b0394ac84251e9e36
chalinor.baliuag <chalinor.baliuag@B106-TUTOR.soit.local>
2016-07-20 09:24:33 +1200

    added another object

--- Person Information/src/MyMain.java
+++ Person Information/src/MyMain.java
@@ -4,6 +4,10 @@ public class MyMain {
 	public static void main(String[] args) {
 		PersonInfo person1 = new PersonInfo ("John Doe", "0123456", "American");
 		person1.displayInfo();
+		
+		PersonInfo person2 = new PersonInfo ("Jane Clark", "987456", "Canadian");
+		person2.displayInfo();
+		
 	}
 
 }
